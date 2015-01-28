b86e0291953cd2811b97ea005a33c4070aa64069
Shahar Talmi <shahar.talmi@gmail.com>
2015-01-28 11:25:00 +0200

    minor fixes

--- templates/common/replace.private.conf.js
+++ templates/common/replace.private.conf.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = {
-  '${enableMocks}': 'false',<% if (dashboardApp) { %>
-  '${metaSiteId}': 'c853c829-503e-48b1-892f-28d8c22a887c'<% } %>
+  '${enableMocks}': 'true',<% if (dashboardApp) { %>
+  '${metaSiteId}': 'c853c829-503e-48b1-892f-28d8c22a887c',<% } %>
+  '${experiments}': '{}'
 };
